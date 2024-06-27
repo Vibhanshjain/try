df0bee16fd95b4236d5ae1788a755dba5a9f36bf
Vibhansh Jain <Vibhanshjain>
2024-06-27 20:41:22 +0530

    jshdf

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// this is sample kjasdhs
 const pianoKeys = document.querySelectorAll(".piano-keys .key"),
 volumeSlider = document.querySelector(".volume-slider input"),
 keysCheckbox = document.querySelector(".keys-checkbox input");
